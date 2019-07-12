4a3db046e556cd9200731e3210e8fbac1aff5e38
Marcus Jones <indelibleink84@gmail.com>
2019-07-12 16:44:16 -0500

    Updated:header- index.js: unknown error that is correct on TL system; updated tab-index.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+const topics = document.querySelector('.topics')
+
+function Tab(topic) {
+    const tab = document.createElement('div')
+    
+    tab.classList.add('tab')
+
+    tab.textContent = topic
+
+    return tab
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(response => {
+    response.data.topics.forEach(topic => {
+        topics.appendChild(Tab(topic))
+    })
+})
